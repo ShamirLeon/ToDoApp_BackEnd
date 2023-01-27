ba3ab12d65f61d1adecfae4f3fd30b0360e08a65
ShamirLeon <leonshamir1@gmail.com>
2023-01-26 20:37:42 -0500

    fix static folder

--- models/server.js
+++ models/server.js
@@ -23,7 +23,7 @@ class Server {
   middlewares () {
     this.app.use(express.json())
     this.app.use(cors())
-    this.app.use(express.static('static'))
+    this.app.use(express.static('public'))
   }
 
   listen () {
