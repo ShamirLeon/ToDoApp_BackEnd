d0383215d61a5d882e919ae43d9a1ac75fa63445
ShamirLeon <leonshamir1@gmail.com>
2023-01-28 14:36:56 -0500

    Database online

--- database/config.js
+++ database/config.js
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose'
 
 const dbConnection = async () => {
-  console.log(process.env.MONGODB_CNX)
-  await mongoose.connect(process.env.MONGODB_CNX)
-  console.log('Base de Datos Online')
-  // try {
-  //   await mongoose.connect(process.env.MONGODB_CNX)
-  //   console.log('Base de Datos Online')
-  // } catch (error) {
-  //   throw new Error('Error al iniciar la Base de Datos')
-  //   console.log(process.env.MONGODB_CNX);
-  // }
+  try {
+    await mongoose.connect(process.env.MONGODB_CNX)
+    console.log('Base de Datos Online')
+  } catch (error) {
+    throw new Error('Error al iniciar la Base de Datos')
+  }
 }
 
 export default dbConnection
